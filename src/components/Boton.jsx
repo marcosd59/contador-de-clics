ff57f86a699283fdf93b1e3c58348a8565956bf4
marcosd59 <damian.marcospool@gmail.com>
2024-01-28 19:19:46 -0500

    Refactor App.js and remove unnecessary files

---
+++ src/components/Boton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Boton({ texto, esBotonDeClic }) {
+  return (
+    <button className={esBotonDeClic ? "boton-clic" : "boton-reiniciar"}>
+      {texto}
+    </button>
+  );
+}
+
+export default Boton;
